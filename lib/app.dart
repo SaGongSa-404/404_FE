ac12aa85be6ef29ae8649830045279e84b6fd5ca
ymin431 <ymin431@naver.com>
2026-04-28 13:43:51 +0900

    refactor: 라우터 파일 분리

--- lib/app.dart
+++ lib/app.dart
@@ -1,57 +1,8 @@
-import 'package:fe_app/features/splash/views/splash_screen.dart';
-import 'package:fe_app/features/wishlist/views/wishlist_consider_screen.dart';
-import 'package:fe_app/features/wishlist/views/wishlist_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-import 'package:fe_app/features/auth/views/login_screen.dart';
-import 'package:fe_app/features/auth/views/signup_screen.dart';
-import 'package:fe_app/features/home/views/home_screen.dart';
-import 'package:fe_app/features/onboarding/views/nickname_screen.dart';
-import 'package:fe_app/features/onboarding/views/budget_screen.dart';
+import 'package:fe_app/core/router/app_router.dart';
 
 final class App extends StatelessWidget {
   const App({super.key});
-
-  static final GoRouter _router = GoRouter(
-    initialLocation: '/',
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const SplashScreen(),
-      ),
-      GoRoute(
-        path: '/login',
-        builder: (context, state) => const LoginScreen(),
-      ),
-      GoRoute(
-        path: '/signup',
-        builder: (context, state) => const SignupScreen(),
-      ),
-      GoRoute(
-        path: '/home',
-        builder: (context, state) => const HomeScreen(),
-      ),
-      GoRoute(
-        path: '/wishlist',
-        builder: (context, state) => const WishlistScreen(),
-        routes:[
-          GoRoute(
-            path: 'consider',
-            builder: (context, state) => const WishlistConsiderScreen(),
-          )
-        ]
-      ),
-      GoRoute(
-        path: '/onboarding/nickname',
-        builder: (context, state) => const NicknameScreen(),
-      ),
-      GoRoute(
-        path: '/onboarding/budget',
-        builder: (context, state) => const BudgetScreen(),
-      ),
-    ],
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -63,7 +14,7 @@ final class App extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         fontFamily: 'Pretendard',
       ),
-      routerConfig: _router,
+      routerConfig: appRouter
     );
   }
 }
