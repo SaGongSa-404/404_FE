f0603b10e8500f3acdbcde5ae14682e0e4cfc700
minseoriii <flower6713@naver.com>
2026-05-06 14:19:11 +0900

    "알람 아이콘 숫자 뱃지 (1~9, 9+) 추가 및 사진 추가"

--- lib/shared/widgets/alarm/alarm_button.dart
+++ lib/shared/widgets/alarm/alarm_button.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fe_app/features/notification/providers/notification_provider.dart';
 
-class AlarmButton extends StatelessWidget {
+class AlarmButton extends ConsumerWidget {
   const AlarmButton({
     super.key,
     required this.onPressed,
@@ -14,17 +16,50 @@ class AlarmButton extends StatelessWidget {
   final Color? color;
 
   @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: SvgPicture.asset(
-        'assets/images/alarm_icon.svg',
-        width: size,
-        height: size,
-        colorFilter: color != null 
-            ? ColorFilter.mode(color!, BlendMode.srcIn) 
-            : null,
-      ),
-      onPressed: onPressed,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final unreadCount = ref.watch(unreadNotificationCountProvider);
+
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        IconButton(
+          icon: SvgPicture.asset(
+            'assets/images/alarm_icon.svg',
+            width: size,
+            height: size,
+            colorFilter: color != null 
+                ? ColorFilter.mode(color!, BlendMode.srcIn) 
+                : null,
+          ),
+          onPressed: onPressed,
+        ),
+        if (unreadCount > 0)
+          Positioned(
+            right: 8,
+            top: 8,
+            child: Container(
+              padding: const EdgeInsets.all(4),
+              decoration: const BoxDecoration(
+                color: Color(0xFFE54327), // 빨간 계열
+                shape: BoxShape.circle,
+              ),
+              constraints: const BoxConstraints(
+                minWidth: 16,
+                minHeight: 16,
+              ),
+              child: Center(
+                child: Text(
+                  unreadCount > 9 ? '9+' : '$unreadCount',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
