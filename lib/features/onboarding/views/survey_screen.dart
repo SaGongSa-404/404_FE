c700beac1573b71d85e4437aad6bef0f415d9490
choijieun <eaee20171@gmail.com>
2026-05-18 03:28:23 +0900

    fix: 온보딩 예산 -> 홈화면으로 경로 변경

--- lib/features/onboarding/views/survey_screen.dart
+++ lib/features/onboarding/views/survey_screen.dart
@@ -35,7 +35,7 @@ class _SurveyScreenState extends ConsumerState<SurveyScreen> {
 
   void _onNext() {
     FocusScope.of(context).unfocus();
-    context.push('/onboarding/wishlist-tutorial');
+    context.push('/onboarding/nugul-intro');
   }
 
   @override
