d1b0ed6206335efdaf232e35232e4cb6fde9b8a9
윰 <132874504+ymin431@users.noreply.github.com>
2026-06-01 01:19:53 +0900

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/fe_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fe_app/MainActivity.kt
@@ -3,6 +3,5 @@ package com.example.fe_app
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
-    // 앱 종료 후 재실행 시 캐시된 엔진이 깨진 상태로 남지 않도록 합니다.
     override fun shouldDestroyEngineWithHost(): Boolean = true
 }
