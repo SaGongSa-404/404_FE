d66ddea8742da0a74300b46aece131624b3c81c2
ymin431 <ymin431@naver.com>
2026-06-19 23:47:53 +0900

    refactor: 뷰레이어, 비즈니스 로직 파일 분리

--- lib/features/wishlist/views/components/wishlist_share_intent_app_shell.dart
+++ lib/features/wishlist/views/components/wishlist_share_intent_app_shell.dart
@@ -1,8 +1,4 @@
-import 'package:fe_app/core/config/env_config.dart';
-import 'package:fe_app/features/auth/models/user.dart';
-import 'package:fe_app/features/auth/providers/auth_provider.dart';
-import 'package:fe_app/features/wishlist/providers/android_share_intent_provider.dart';
-import 'package:fe_app/features/wishlist/viewmodels/wishlist_viewmodel.dart';
+import 'package:fe_app/features/wishlist/providers/wishlist_share_intent_coordinator_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -24,67 +20,33 @@ class WishlistShareIntentAppShell extends ConsumerStatefulWidget {
 
 class _WishlistShareIntentAppShellState
     extends ConsumerState<WishlistShareIntentAppShell> {
-  bool _flushScheduled = false;
-  bool _isHandlingShare = false;
-
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (!mounted) return;
-      ref.read(androidShareIntentProvider.notifier).start();
-      _flushPendingShare();
+      ref.read(wishlistShareIntentCoordinatorProvider.notifier).start();
     });
   }
 
-  void _scheduleFlush() {
-    if (_flushScheduled) return;
-    _flushScheduled = true;
-
+  void _openShareAfterNavigation(String sharedLink) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _flushScheduled = false;
-      if (!mounted) return;
-      _flushPendingShare();
-    });
-  }
-
-  bool _canHandleShare(AsyncValue<UserModel?> authState) {
-    if (authState.isLoading || authState.hasError) return false;
-    if (EnvConfig.isDevXUserIdAuth) return true;
-    return authState.valueOrNull?.onboardingStatus == 'COMPLETED';
-  }
-
-  void _flushPendingShare() {
-    if (_isHandlingShare) return;
-
-    final sharedLink = ref.read(androidShareIntentProvider);
-    if (sharedLink == null || sharedLink.isEmpty) return;
-
-    if (!_canHandleShare(ref.read(authProvider))) return;
-
-    _isHandlingShare = true;
-    ref.read(androidShareIntentProvider.notifier).consume();
-    widget.router.go('/wishlist');
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _isHandlingShare = false;
       if (!mounted) return;
       ref
-          .read(wishlistViewModelProvider.notifier)
-          .openAddPanelWithLink(sharedLink);
+          .read(wishlistShareIntentCoordinatorProvider.notifier)
+          .openPendingShare(sharedLink);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    ref.listen<String?>(androidShareIntentProvider, (previous, next) {
-      if (next != null && next.isNotEmpty) {
-        _scheduleFlush();
-      }
-    });
-
-    ref.listen<AsyncValue<UserModel?>>(authProvider, (previous, next) {
-      _scheduleFlush();
+    ref.listen<String?>(wishlistShareIntentCoordinatorProvider, (_, next) {
+      if (next == null || next.isEmpty) return;
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (!mounted) return;
+        widget.router.go('/wishlist');
+        _openShareAfterNavigation(next);
+      });
     });
 
     return widget.child;
