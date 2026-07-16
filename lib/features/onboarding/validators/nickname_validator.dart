3c7a91bf9f7edece40e53d6dc001d234b82ac71d
choijieun <eaee20171@gmail.com>
2026-07-16 18:57:27 +0900

    fix: 닉네임 입력 validator 수정

--- lib/features/onboarding/validators/nickname_validator.dart
+++ lib/features/onboarding/validators/nickname_validator.dart
@@ -26,8 +26,9 @@ class NicknameValidator {
   static const minLength = 2;
   static const maxLength = 8;
 
-  static final _allowedPattern =
-      RegExp(r'^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]+$');
+  // 완성형 한글(가-힣) + 영문 + 숫자만 허용.
+  // 단독 자모(ㄱ-ㅎ, ㅏ-ㅣ)는 백엔드가 거부하므로 여기서도 막아 입력 단계에서 즉시 걸러낸다.
+  static final _allowedPattern = RegExp(r'^[가-힣a-zA-Z0-9]+$');
   static final _whitespacePattern = RegExp(r'\s');
 
   const NicknameValidator._();
